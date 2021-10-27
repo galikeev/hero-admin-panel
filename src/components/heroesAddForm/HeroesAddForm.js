2e6441ac495f67c48cecac12e6ad0aac4eaaff08
Ramil <vk2011@bk.ru>
2021-10-27 22:07:54 +0500

    feat: add Redux-toolkit - createSlice

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,19 +1,9 @@
-// Задача для этого компонента:
-// Реализовать создание нового героя с введенными данными. Он должен попадать
-// в общее состояние и отображаться в списке + фильтроваться
-// Уникальный идентификатор персонажа можно сгенерировать через uiid
-// Усложненная задача:
-// Персонаж создается и в файле json при помощи метода POST
-// Дополнительно:
-// Элементы <option></option> желательно сформировать на базе
-// данных из фильтров
-
 import { useHttp } from "../../hooks/http.hook";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
 
-import { heroCreated } from "../../actions";
+import { heroCreated } from "../heroesList/heroesSlice";
 
 const HeroesAddForm = () => {
     const [heroName, setHeroName] = useState('');
@@ -54,6 +44,7 @@ const HeroesAddForm = () => {
         if (filters && filters.length > 0) {
             return filters.map(({name, label}) => {
                 if (name === 'all') {
+                    // eslint-disable-next-line
                     return;
                 }
                 return <option key={name} value={name}>{label}</option>
