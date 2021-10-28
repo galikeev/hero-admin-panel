528c89909780609537f2a423cda84a671d05541f
Ramil <vk2011@bk.ru>
2021-10-28 10:58:09 +0500

    refactor: add Redux-toolkit - createEntityAdapter

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -9,13 +9,14 @@ import { useHttp } from "../../hooks/http.hook";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import classNames from "classnames";
-
-import { activeFilterChanged, fetchFilters } from "./filtersSlice";
+import store from '../../store';
+import { activeFilterChanged, fetchFilters, selectAll } from "./filtersSlice";
 import Spinner from "../spinner/Spinner";
 
 const HeroesFilters = () => {
 
-    const {filters, filtersLoadingStatus, activeFilter} = useSelector(state => state.filters);
+    const {filtersLoadingStatus, activeFilter} = useSelector(state => state.filters);
+    const filters = selectAll(store.getState());
     const dispatch = useDispatch();
     const {request} = useHttp();
 
